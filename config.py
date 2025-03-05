d34a21014ef3a419a5de1428d6b8b07ffeb398e5
mipfikus <info@mishkadesign.ru>
2025-03-05 12:51:21 +0300

    Настроили переменное окружение

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN:str = getenv("TOKEN")
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import asyncio
 import logging
 import sys
-from os import getenv
-from dotenv import load_dotenv
-load_dotenv()
-
+from config import TOKEN
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
-TOKEN = getenv("TOKEN")
 
 dp = Dispatcher()
 
@@ -29,11 +24,9 @@ async def echo_handler(message: Message) -> None:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
-
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
