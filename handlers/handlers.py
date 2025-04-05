cafd247b7e118fb10efce8955736135912f4b11a
mipfikus <info@mishkadesign.ru>
2025-04-05 20:00:48 +0300

    Сделали рабочие /start, /help, /status + Keyboard + BotCommands

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,26 +1,48 @@
-__all__ = [
-    'register_message_handlers'
-]
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from aiogram import Router, types, html
 
+from .callbacks import send_random_value
+from .keyboard import get_random_keyboard
 
-# TODO - Опишите вызов функций обработчиков через маршрутизацию
-# Работа c Router - https://docs.aiogram.dev/en/v3.14.0/dispatcher/router.html
-# Пример работы с Router через декораторы @router - https://mastergroosha.github.io/aiogram-3-guide/routers/
-# Пример работы с Router через функцию сборщик https://stackoverflow.com/questions/77809738/how-to-connect-a-router-in-aiogram-3-x-x#:~:text=1-,Answer,-Sorted%20by%3A
+router = Router()
 
+@router.message(CommandStart()) # /start
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!")
 
-from aiogram import types, Router
-from aiogram.filters import Command
-from .keyboard import keyboard  # импорт из клавиатур
-from .callbacks import callback_message  # импорт из коллбека
+@router.message(Command("status")) # /status
+async def command_status_handler(message: Message) -> None:
+    await message.answer(f"username: {html.bold(message.from_user.username)}, id: {html.bold(message.from_user.id)}")
 
+@router.message(Command("help")) # /help
+async def command_help_handler(message: Message) -> None:
+    help_text = f"""
+Привет! Я бот. Вот список доступных команд:
 
-async def process_start_command(message: types.Message):
-    '''Команда start'''
-    await message.answer(text="Привет!")
+/start - Запускает бота и выводит информацию о пользователе.
+/help - Выводит справочную информацию о боте и доступных командах.
+/status - Выводит ID и имя пользователя.
+/random - Команда выводит кнопку ответ на нажатие которой - сообщение
 
+Разработчики: @yk_rf228, @pashkabesik
+    """
+    await message.answer(help_text)
 
-# Здесь описывается маршрутизация
-def register_message_handlers():
-    '''Маршрутизация обработчиков'''
-    pass
+@router.message(Command("random"))  # /random - команда в которой используется кнопка под сообщением
+async def cmd_random(message: types.Message):
+    keyboard = get_random_keyboard()  # Получаем клавиатуру
+    await message.answer(
+        "Нажмите на кнопку, чтобы бот отправил кое-что",
+        reply_markup=keyboard
+    )
+
+@router.message()
+async def echo_handler(message: Message) -> None:
+
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
--- main.py
+++ main.py
@@ -1,33 +1,25 @@
-# version 0.1.0
-# ПАША БЕССОНОВ, АЛИНА, ЭВЕЛИНА
+# version 0.2.0
 import asyncio
 import logging
 import sys
 from config import TOKEN
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+
+from handlers import handlers
+from handlers import callbacks
+from handlers.bot_commands import set_commands
 
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}!")
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    dp.include_router(handlers.router)
+    dp.include_router(callbacks.router)
+    dp.startup.register(set_commands)
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
