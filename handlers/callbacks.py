e4dd9d883eb9c0c39b8e0e6374a2e5c4fe79c23e
PavelBessonov228 <89220292577a@gmail.com>
2025-04-30 05:46:00 +0300

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -5,7 +5,6 @@ router = Router()
 
 @router.callback_query(F.data == "random_value")
 async def send_random_value(callback: types.CallbackQuery):
-    await callback.answer()
     await callback.message.answer('кое-что')
     logging.info(f"user {callback.from_user.id} gets random value ")
 
